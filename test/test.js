eb61fda6ab466f080cc3b788c481eb0541ef6f5d
Nathan White <nw@nwhite.net>
2014-03-19 08:32:32 -0600

    tweaking... oo

---
+++ test/test.js
@@ -0,0 +1,16 @@
+var Scheduler = require('..')
+  , util = require('../lib/util');
+  
+  
+  [31].forEach(function(students){
+    log(new Scheduler(util.createStudents(students)))
+  });
+  
+
+function log(assignment){
+  util.scoreRooms(assignment.rooms, assignment._idx).forEach(function(score, idx){
+    console.log("room-"+ idx + " score: " +score, assignment.rooms[idx])
+  })
+  console.log('------------------------------------')
+  
+}
